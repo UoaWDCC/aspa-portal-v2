41af9598d2018059724a5b9ba620d8dc9f5dcbcb
Matthew Tao <43839811+matthewyingtao@users.noreply.github.com>
2022-05-09 16:17:37 +1200

    add tailwind example use

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* Add classes like this to an element instead of in the CSS file */}
+        <p className="text-xl text-red-200 tracking-widest uppercase">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
