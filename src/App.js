71feab24d97c3951576fb24d5e65431839c62df4
Matthew Tao <43839811+matthewyingtao@users.noreply.github.com>
2022-05-09 17:05:30 +1200

    implemented react-router example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* Add classes like this to an element instead of in the CSS file */}
-        <p className="text-xl text-red-200 tracking-widest uppercase">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
+    </Routes>
   );
 }
-
-export default App;
