bd6304fffbbd53734be736e77f33c041180743e0
LX6T <alex@pullen.net.nz>
2022-05-23 13:27:03 +1200

    Create footer, import Poppin font

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+export default function Footer() {
+  return (
+    <nav className="absolute font-['Poppins'] font-normal text-sm bottom-0 ml-10 mb-8 leading-10">
+      <ul>
+        <li>
+          <Link to="/about">about</Link>
+        </li>
+        <li>
+          <Link to="/contact">contact us</Link>
+        </li>
+        <li>
+          <Link to="/register">terms and conditions</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
