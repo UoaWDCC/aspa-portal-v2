79ebe71c25814177a941204c6f2efca226b33902
Tanishq <tanishq.ranjith00@gmail.com>
2022-07-29 11:27:01 +1200

    Connected MongoDB

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,15 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
+const dbURL = process.env.DB_URL || "mongodb+srv://<username>:<password>@cluster0.3rsko.mongodb.net/?retryWrites=true&w=majority";
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
 // get driver connection
 const dbo = require("./db/conn");
+
+mongoose.connect(dbURL).catch(error => console.log('Error'));
+
 
 app.listen(port, () => {
   // perform a database connection when server starts
