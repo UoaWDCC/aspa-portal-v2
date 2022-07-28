4e73a5bfb092b8b704797a12390c72d9e76be12e
Matthew Tao <43839811+matthewyingtao@users.noreply.github.com>
2022-07-29 11:26:58 +1200

    Finish Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,23 @@
+import { IoSearch } from "react-icons/io5";
 import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">Dashboard</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        <li>
-          <Link to="/upcoming-events">Upcoming Events</Link>
-        </li>
-      </ul>
+    <nav className="flex justify-between px-12 py-6 sans">
+      <div className="flex gap-8">
+        <span className="font-bold">ASPA</span>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/upcoming-events">Upcoming Events</Link>
+      </div>
+      <div className="flex gap-8 font-bold items-center">
+        <Link to="/" className="flex gap-2 items-center">
+          <IoSearch size="20" />
+          <span>Search</span>
+        </Link>
+        <Link to="/about">Login</Link>
+        <Link to="/upcoming-events">Register</Link>
+      </div>
     </nav>
   );
 }
