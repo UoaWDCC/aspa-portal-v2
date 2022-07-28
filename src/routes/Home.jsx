7a70c621e4fc61302d6674fabd120165a17572c5
Matthew Tao <43839811+matthewyingtao@users.noreply.github.com>
2022-07-29 11:36:27 +1200

    create home page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex flex-col gap-12 justify-center items-center h-full">
+      <h1 className="text-7xl text-center">
+        Auckland Students <br /> Pool Association
+      </h1>
+      <div className="grid gap-8 grid-cols-2 text-lg">
+        <button className="border-white border-2 px-6 py-2">View Events</button>
+        <button className="border-white bg-white text-gray-800 border-2 px-6 py-2">
+          Join Us
+        </button>
+      </div>
     </div>
   );
 }
