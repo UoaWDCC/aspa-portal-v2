de9b01b426f7a13294bd88e76d5e08e9d98e3d1c
Youting <youting123456@gmail.com>
2022-07-29 11:55:52 +1200

    Changed exports to type: module

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
 const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 const dbURL =
   process.env.DB_URL ||
   "mongodb+srv://123:123@cluster0.3rsko.mongodb.net/?retryWrites=true&w=majority";
 
-const userRoute = require("./routes/user.js");
+import userRoute from "./routes/user.js";
 
 app.use(cors());
 app.use(express.json());
