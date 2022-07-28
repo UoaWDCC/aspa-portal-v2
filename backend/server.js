889fd4eb21d740a096a2639697bacbf50e9d443c
Tanishq <tanishq.ranjith00@gmail.com>
2022-07-29 11:27:01 +1200

    Connected MongoDB

--- backend/server.js
+++ backend/server.js
@@ -12,14 +12,15 @@ import eventRoute from "./routes/event.js";
 
 app.use(cors());
 app.use(express.json());
-
-app.use("/event", eventRoute);
-
-mongoose
-  .connect(dbURL)
-  .then(() => console.log(`Connected to database`))
-  .catch((error) => console.log(error));
+app.use(require("./routes/record"));
+// get driver connection
+const dbo = require("./db/conn");
 
 app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+
+  });
   console.log(`Server is running on port: ${port}`);
 });
