ec8883b09c3dcaae0e9d0932e58a337c71fe9eeb
Youting <youting123456@gmail.com>
2022-07-29 11:37:10 +1200

    Connected to Mongoose successfully (fr this time)

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,20 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
-const dbURL = process.env.DB_URL || "mongodb+srv://<username>:<password>@cluster0.3rsko.mongodb.net/?retryWrites=true&w=majority";
+const dbURL =
+  process.env.DB_URL ||
+  "mongodb+srv://123:123@cluster0.3rsko.mongodb.net/?retryWrites=true&w=majority";
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/conn");
 
-mongoose.connect(dbURL).catch(error => console.log('Error'));
-
+mongoose
+  .connect(dbURL)
+  .then(() => console.log(`Connected to database`))
+  .catch((error) => console.log(error));
 
 app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
-
-  });
   console.log(`Server is running on port: ${port}`);
 });
