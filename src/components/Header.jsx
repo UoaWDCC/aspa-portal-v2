b6b6b46c52c01c91f05f1260c9a6cc4b13104162
Matthew Tao <43839811+matthewyingtao@users.noreply.github.com>
2022-07-29 13:34:10 +1200

    Add logo to navbar (#29)
    * add logo
    
    * add logo to navbar
    
    * fix broken link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import { IoSearch } from "react-icons/io5";
 import { Link } from "react-router-dom";
+import Logo from "../assets/logo.svg";
 
 export default function Header() {
   return (
     <nav className="flex justify-between px-12 py-6 sans">
-      <div className="flex gap-8">
-        <span className="font-bold">ASPA</span>
-        <Link to="/">Home</Link>
+      <div className="flex gap-8 items-center">
+        <Link to="/" className="flex gap-4 items-center">
+          <img src={Logo} alt="" className="h-8" />
+          <span className="font-bold text-lg">ASPA</span>
+        </Link>
         <Link to="/about">About</Link>
         <Link to="/upcoming-events">Upcoming Events</Link>
       </div>
