6c5ccfc3a95f9e64b10d8846a5c0e4b95e194588
marissalll <marissaaa.lll123@gmail.com>
2022-07-29 14:34:14 +1200

    about stuff

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -2,28 +2,30 @@ import MyImage from "./UpcomingEvents/images/ASPA_logo_inverted.png";
 
 export default function About() {
   return (
-    <>
-      <div className="mx-32 py-28 prose text-white">
-        <div className="w-3/4 overflow-x-auto">
+    <div className="mx-32 py-28">
+      <div className="flex">
+        <div className="prose text-white ">
           <h5 className="text-white leading-none mb-2">WHO ARE WE?</h5>
-          <h1 className="text-white leading-none">
-            Auckland Student Pool Association
-          </h1>
-          <p className="max-w-[75ch]">
-            The Auckland Student Pool Association (ASPA), based in the
-            University of Auckland, focuses on faciliating a supporting
-            environment that brings people together who are passionate about
-            pool and aims to promote billiard sports. We hold casual events,
-            coaching events, and tournaments, connecting members that are
-            interested in playing against other members in a competitive and
-            social environment.
-          </p>
+          <div className="w-3/4 overflow-x-auto">
+            <h1 className="text-white leading-none text-5xl">
+              Auckland Student Pool Association
+            </h1>
+          </div>
+        </div>
+        <div className="justify-center items-center h-64 w-64">
+          <img src={MyImage} alt="logo" />
         </div>
       </div>
-
       <div>
-        <img src={MyImage} alt="logo" />
+        <p className="max-w-[100ch] text-2xl">
+          The Auckland Student Pool Association (ASPA), based in the University
+          of Auckland, focuses on faciliating a supporting environment that
+          brings people together who are passionate about pool and aims to
+          promote billiard sports. We hold casual events, coaching events, and
+          tournaments, connecting members that are interested in playing against
+          other members in a competitive and social environment.
+        </p>
       </div>
-    </>
+    </div>
   );
 }
