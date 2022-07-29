28f47c5a8a08116d8bd1074b7527aa4e8fa25819
Tanishq <tanishq.ranjith00@gmail.com>
2022-07-29 12:28:43 +1200

    Created a post request for events

--- backend/controllers/event.js
+++ backend/controllers/event.js
@@ -8,3 +8,19 @@ export const getEvents = async (req, res) => {
     console.log(error);
   }
 };
+
+export const createEvent = async (req, res) => {
+  try {
+    // const eventData = req.body;
+    const event = new Event({
+      eventTitle: "Event 1",
+      eventDescription: "Event 1 desc",
+      eventLocation: "OGGB",
+      eventDate: new Date(),
+    });
+    await event.save();
+    res.status(201).json(event);
+  } catch (error) {
+    res.json(error);
+  }
+};
--- backend/routes/event.js
+++ backend/routes/event.js
@@ -4,5 +4,6 @@ import * as Event from "../controllers/event.js";
 const router = express.Router();
 
 router.get("/", Event.getEvents);
+router.post("/", Event.createEvent);
 
 export default router;
