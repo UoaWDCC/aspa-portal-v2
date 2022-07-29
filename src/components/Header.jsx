c65e760726e82299ece838af160a5846bc2890c0
Matthew Tao <43839811+matthewyingtao@users.noreply.github.com>
2022-07-29 12:16:44 +1200

    fix broken link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,8 +18,8 @@ export default function Header() {
           <IoSearch size="20" />
           <span>Search</span>
         </Link>
-        <Link to="/about">Login</Link>
-        <Link to="/upcoming-events">Register</Link>
+        <Link to="/">Login</Link>
+        <Link to="/register">Register</Link>
       </div>
     </nav>
   );
