cbf48df0ae1d770114f7a54fab6c276e011a91b7
Matthew Tao <43839811+matthewyingtao@users.noreply.github.com>
2022-08-05 12:25:41 +1200

    added hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,57 @@
+import { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { IoSearch } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import Logo from "../assets/logo.svg";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav className="flex justify-between px-12 py-6 sans">
-      <div className="flex gap-8 items-center">
-        <Link to="/" className="flex gap-4 items-center">
-          <img src={Logo} alt="" className="h-8" />
-          <span className="font-bold text-lg">ASPA</span>
-        </Link>
-        <Link to="/about">About</Link>
-        <Link to="/upcoming-events">Upcoming Events</Link>
-      </div>
-      <div className="flex gap-8 font-bold items-center">
-        <Link to="/" className="flex gap-2 items-center">
-          <IoSearch size="20" />
-          <span>Search</span>
-        </Link>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
-      </div>
-    </nav>
+    <>
+      <nav className="flex md:justify-between px-12 py-6 sans">
+        <div className="flex gap-8 w-full items-center justify-between md:justify-start md:w-auto">
+          <Link to="/" className="flex gap-4 items-center">
+            <img src={Logo} alt="" className="h-8" />
+            <span className="font-bold text-lg">ASPA</span>
+          </Link>
+          <GiHamburgerMenu
+            className="relative block md:hidden z-50 cursor-pointer"
+            onClick={() => setIsOpen((v) => !v)}
+            size={20}
+          />
+          <Link className="hidden md:block" to="/about">
+            About
+          </Link>
+          <Link className="hidden md:block" to="/upcoming-events">
+            Upcoming Events
+          </Link>
+        </div>
+        <div className="hidden gap-8 font-bold items-center md:flex">
+          <Link to="/" className="flex gap-2 items-center">
+            <IoSearch size="20" />
+            <span>Search</span>
+          </Link>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </div>
+      </nav>
+      {isOpen && (
+        <div className="fixed inset-0 bg-gray-900 flex flex-col justify-center items-center p-4 gap-8 text-xl z-40 md:hidden">
+          <Link onClick={() => setIsOpen(false)} to="/about">
+            About
+          </Link>
+          <Link onClick={() => setIsOpen(false)} to="/upcoming-events">
+            Upcoming Events
+          </Link>
+          <Link onClick={() => setIsOpen(false)} to="/login">
+            Login
+          </Link>
+          <Link onClick={() => setIsOpen(false)} to="/register">
+            Register
+          </Link>
+        </div>
+      )}
+    </>
   );
 }
--- src/routes/UpcomingEvents/index.jsx
+++ src/routes/UpcomingEvents/index.jsx
@@ -1,10 +1,10 @@
-import Heading from "./Heading";
 import EventBoxLeft from "./EventBoxLeft";
 import EventBoxRight from "./EventBoxRight";
+import Heading from "./Heading";
 
 export default function UpcomingEvents() {
   return (
-    <div>
+    <div className="isolate">
       <Heading />
       <div className="flex flex-col gap-8 p-8 justify-center items-center">
         <EventBoxLeft />
