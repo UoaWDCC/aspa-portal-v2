6cf633ea993250603af3bc74c0e7bd092679e72c
Youting <youting123456@gmail.com>
2022-08-26 12:38:39 +1200

    Configured dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import * as dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 const dbURL =
