29d9c39df18a7c32a057a9150f1d77b57adcda4b
LX6T <alex@pullen.net.nz>
2022-09-16 12:10:42 +1200

    Fixed getUser API, added getUsers route

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,10 +1,23 @@
 import User from "../models/user.js";
 
+export const getUsers = async (req, res) => {
+  console.log("GET ALL USERS");
+  try {
+    const users = await User.find({});
+    console.log(users);
+    res.json(users);
+  } catch (e) {
+    res.status(400).json({ message: e.message });
+  }
+};
+
 export const getUser = async (req, res) => {
   try {
-    const email = req.params;
-    const user = await User.find({ email: email.toLowerCase() });
-    res.json(user);
+    const email = req.params.email;
+    console.log("GET USER " + email);
+    const user = await User.find({ email });
+    console.log(user[0]);
+    res.json(user[0]);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
@@ -15,15 +28,17 @@ export const createUser = async (req, res) => {
     // CHECK IF USER DETAILS ARE VALID FIRST (todo)
 
     const userDetails = req.body;
+    console.log(`CREATE USER ${userDetails.firstName} ${userDetails.lastName}`);
     const user = new User({
       firstName: userDetails.firstName,
       lastName: userDetails.lastName,
       email: userDetails.email,
-      UPI: userDetails.upi,
+      UPI: userDetails.UPI,
       skillLevel: userDetails.skillLevel,
       previousMember: userDetails.previousMember,
     });
     await user.save();
+    console.log(user);
     res.json(user);
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -32,12 +47,11 @@ export const createUser = async (req, res) => {
 
 export const editUser = async (req, res) => {
   try {
-    const email = req.params;
+    const email = req.params.email;
+    console.log("EDIT USER " + email);
     const userData = req.body;
-    const user = await User.findOneAndUpdate(
-      { email: email.toLowerCase() },
-      userData
-    );
+    const user = await User.findOneAndUpdate({ email: email }, userData);
+    console.log(user);
     res.json(user);
   } catch (error) {
     res.status(404).json({ message: error.message });
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,6 +3,7 @@ import * as User from "../controllers/user.js";
 
 const router = express.Router();
 
+router.get("/", User.getUsers);
 router.get("/:email", User.getUser);
 router.post("/", User.createUser);
 router.patch("/:email", User.editUser);
