fa86df8ce728d75f3e9f3145cfb0f63c41e6c8ed
NoamBechhofer <noam.bechhofer@gmail.com>
2023-04-13 00:34:19 +1200

    user model, controller and routes

--- packages/backend/routes/user.js
+++ packages/backend/routes/user.js
@@ -3,6 +3,11 @@ import * as User from "../controllers/user.js";
 
 const router = express.Router();
 
-router.get("/", User.getUser);
+router.get("/", User.getUsers);                         // good
+router.get("/:userId", User.getUser);                   // good
+router.post("/", User.createUser);                      // good
+router.patch("/:userId", User.updateUser);              // good
+router.patch("/addevent/:userId", User.addEvent);
+router.delete("/:userId", User.deleteUser);             // good
 
 export default router;
