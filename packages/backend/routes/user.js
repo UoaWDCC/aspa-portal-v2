c20b0526414feabfecc345e36204ea4343e9f3c2
Noam Bechhofer <noam.bechhofer@gmail.com>
2023-04-13 00:48:29 +1200

    decommented
    removed extraneous comments, oops

--- packages/backend/routes/user.js
+++ packages/backend/routes/user.js
@@ -3,11 +3,11 @@ import * as User from "../controllers/user.js";
 
 const router = express.Router();
 
-router.get("/", User.getUsers);                         // good
-router.get("/:userId", User.getUser);                   // good
-router.post("/", User.createUser);                      // good
-router.patch("/:userId", User.updateUser);              // good
+router.get("/", User.getUsers);
+router.get("/:userId", User.getUser);
+router.post("/", User.createUser);
+router.patch("/:userId", User.updateUser);
 router.patch("/addevent/:userId", User.addEvent);
-router.delete("/:userId", User.deleteUser);             // good
+router.delete("/:userId", User.deleteUser);
 
 export default router;
