f5c57a2fb1c48e2c84d3a832253276e7b7e58a7c
JACE <jacexin77@gmail.com>
2023-04-20 11:48:33 +1200

    changed register form design

--- packages/frontend/src/routes/NewForm.jsx
+++ packages/frontend/src/routes/NewForm.jsx
@@ -1,48 +1,89 @@
 import { useState } from "react";
+import "./FormStyles.css";
 
 const Create = () => {
-  const [name, setName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [paymentType, setPaymentType] = useState("");
   const [isPending, setIsPending] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const player = { name, email, paymentType };
+    const player = { firstName, lastName, email, paymentType };
     console.log(player);
     setIsPending(true);
     // Make API call or perform other actions here
   };
 
   return (
-    <div className="create block max-w-md rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+    <div className="block p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 form">
       {/* Info */}
-      <h2>Register</h2>
+      <h1 className="title">Registration Form</h1>
+      <br />
       <p>
         Bring your cue skills, friends, and pool rivals for a casual night of
         pool! Limited availability so sign up to confirm your spot! (This form
         will close upon reaching capacity so if you're reading this it's not too
         late :D)
       </p>
+      <br />
       <p>Thursday 19th May 2022, 6:30 - 8:00 PM</p>
+      <br />
       <p>9 City Road, Orange Pool Club</p>
+      <br />
       <p>$6.00 with ASPA membership</p>
+      <br />
 
       <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Full Name</label>
-          <input
-            type="text"
-            id="name"
-            required
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
+        <div className="grid grid-cols-2 gap-4 width">
+          <div className="mb-6">
+            <label
+              htmlFor="firstName"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              FIrst name
+            </label>
+            <input
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="John"
+              type="text"
+              id="firstName"
+              required
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-6">
+            <label
+              htmlFor="lastName"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Last name
+            </label>
+            <input
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Doe"
+              type="text"
+              id="lastName"
+              required
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
         </div>
 
-        <div>
-          <label htmlFor="email">Email</label>
+        <div className="mb-6">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Email
+          </label>
           <input
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="name@email.com"
             type="email"
             id="email"
             required
@@ -51,9 +92,15 @@ const Create = () => {
           />
         </div>
 
-        <div>
-          <label htmlFor="paymentType">Payment Type:</label>
+        <div className="mb-6">
+          <label
+            htmlFor="paymentType"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Payment Type:
+          </label>
           <select
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             id="paymentType"
             value={paymentType}
             onChange={(e) => setPaymentType(e.target.value)}
@@ -63,7 +110,14 @@ const Create = () => {
           </select>
         </div>
 
-        {!isPending && <button type="submit">Register</button>}
+        {!isPending && (
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Register
+          </button>
+        )}
         {isPending && <button disabled>Registering...</button>}
       </form>
     </div>
