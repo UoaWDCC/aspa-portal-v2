d815b4a7506ecc4b0d66490c4bfb2518c9b6de6b
Connor <cste303@aucklanduni.ac.nz>
2023-05-01 12:06:40 +1200

    Created verifyToken middleware and protection for register routes

--- packages/backend/src/register/register-route.ts
+++ packages/backend/src/register/register-route.ts
@@ -4,8 +4,12 @@ import {
   removeRegistration,
   updatePaymentStatus,
 } from "./register-controller";
+const verifyToken = require("../middleware/verifyToken");
 
 const router = express.Router();
+
+// verify the token for all register routes
+router.use(verifyToken);
 
 router.post("/", registerUserEvent);
 router.delete("/:userId/:eventId", removeRegistration);
