0641ead3a74aa09e53e9367b6bb98b74a950f8d1
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2023-05-11 11:55:05 +1200

    refactor: modify to userId

--- packages/backend/src/middleware/verifyToken.ts
+++ packages/backend/src/middleware/verifyToken.ts
@@ -4,22 +4,22 @@ const User = require("../user/user-model");
 const admin = require("firebase-admin");
 
 const verifyToken = async (req: any, res: any, next: NextFunction) => {
-  const authToken = req.headers.authorization?.split(" ")[1];
+    const authToken = req.headers.authorization?.split(" ")[1];
 
-  if (!authToken) {
-    // No authentication token: assume guest user
-    req.user = null;
-    return next();
-  }
+    if (!authToken) {
+        // No authentication token: assume guest user
+        req.userId = null;
+        return next();
+    }
 
-  try {
-    const decodedToken = await admin.auth().verifyIdToken(authToken);
-    req.userId = decodedToken.uid;
-    next();
-  } catch (error) {
-    console.log(error);
-    return res.status(401).json({ error: "You are not authorized" });
-  }
+    try {
+        const decodedToken = await admin.auth().verifyIdToken(authToken);
+        req.userId = decodedToken.uid;
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.status(401).json({ error: "You are not authorized" });
+    }
 };
 
 module.exports = verifyToken;
