55e6a4f7fbd5eee5199ff39a7ddce765b4f38506
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2023-05-11 12:40:54 +1200

    feat: update to use uuid for guest

--- packages/backend/src/event/event-controller.ts
+++ packages/backend/src/event/event-controller.ts
@@ -1,97 +1,104 @@
 import { Event, RegistrationRecordEvent } from "./event-model";
 import { Request, Response } from "express";
 import mongoose from "mongoose";
+import { v4 as uuidv4 } from "uuid";
 
 export const getEvents = async (req: Request, res: Response) => {
-  try {
-    const events = await Event.find({});
-    res.status(200).json(events);
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const events = await Event.find({});
+        res.status(200).json(events);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export const getEvent = async (req: Request, res: Response) => {
-  try {
-    const { eventId } = req.params;
+    try {
+        const { eventId } = req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(eventId)) {
-      return res.status(404).json({ error: "No such event" });
+        if (!mongoose.Types.ObjectId.isValid(eventId)) {
+            return res.status(404).json({ error: "No such event" });
+        }
+
+        const event = await Event.findById(eventId);
+
+        if (!event) {
+            return res.status(404).json({ error: "No such event" });
+        }
+
+        res.status(200).json(event);
+    } catch (error) {
+        console.log(error);
     }
-
-    const event = await Event.findById(eventId);
-
-    if (!event) {
-      return res.status(404).json({ error: "No such event" });
-    }
-
-    res.status(200).json(event);
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 export const createEvent = async (req: Request, res: Response) => {
-  try {
-    if (
-      req.body.eventTitle &&
-      req.body.eventDescription &&
-      req.body.eventLocation &&
-      req.body.eventTime
-    ) {
-      const event = new Event({
-        eventTitle: req.body.eventTitle,
-        eventDescription: req.body.eventDescription,
-        eventLocation: req.body.eventLocation,
-        eventTime: new Date(req.body.eventTime),
-        eventLink: req.body.eventLink,
-      });
-      await event.save();
-      res.status(201).json(event);
-    } else {
-      return res.status(400).json({ error: "Please fill in all the fields" });
+    try {
+        if (
+            req.body.eventTitle &&
+            req.body.eventDescription &&
+            req.body.eventLocation &&
+            req.body.eventTime
+        ) {
+            const event = new Event({
+                eventTitle: req.body.eventTitle,
+                eventDescription: req.body.eventDescription,
+                eventLocation: req.body.eventLocation,
+                eventTime: new Date(req.body.eventTime),
+                eventLink: req.body.eventLink,
+            });
+            await event.save();
+            res.status(201).json(event);
+        } else {
+            return res
+                .status(400)
+                .json({ error: "Please fill in all the fields" });
+        }
+    } catch (error) {
+        console.error(error);
+        res.json(error);
     }
-  } catch (error) {
-    console.error(error);
-    res.json(error);
-  }
 };
 
 export const updateEvent = async (req: Request, res: Response) => {
-  try {
-    const { eventId } = req.params;
+    try {
+        const { eventId } = req.params;
 
-    const eventData = await Event.findById(eventId);
-    if (eventData != null) {
-      const event = await Event.findByIdAndUpdate(
-        { _id: eventId },
-        {
-          eventTitle:
-            req.body.eventTitle != ""
-              ? req.body.eventTitle
-              : eventData.eventTitle,
-          eventDescription:
-            req.body.eventDescription != ""
-              ? req.body.eventDescription
-              : eventData.eventDescription,
-          eventLocation:
-            req.body.eventLocation != ""
-              ? req.body.eventLocation
-              : eventData.eventLocation,
-          eventTime:
-            req.body.eventTime != "" ? req.body.eventTime : eventData.eventTime,
-          eventLink:
-            req.body.eventLink != "" ? req.body.eventLink : eventData.eventLink,
+        const eventData = await Event.findById(eventId);
+        if (eventData != null) {
+            const event = await Event.findByIdAndUpdate(
+                { _id: eventId },
+                {
+                    eventTitle:
+                        req.body.eventTitle != ""
+                            ? req.body.eventTitle
+                            : eventData.eventTitle,
+                    eventDescription:
+                        req.body.eventDescription != ""
+                            ? req.body.eventDescription
+                            : eventData.eventDescription,
+                    eventLocation:
+                        req.body.eventLocation != ""
+                            ? req.body.eventLocation
+                            : eventData.eventLocation,
+                    eventTime:
+                        req.body.eventTime != ""
+                            ? req.body.eventTime
+                            : eventData.eventTime,
+                    eventLink:
+                        req.body.eventLink != ""
+                            ? req.body.eventLink
+                            : eventData.eventLink,
+                }
+            );
+
+            res.status(200).json(event);
+        } else {
+            return res.status(404).json({ error: "No such event" });
         }
-      );
-
-      res.status(200).json(event);
-    } else {
-      return res.status(404).json({ error: "No such event" });
+    } catch (error) {
+        res.json(error);
     }
-  } catch (error) {
-    res.json(error);
-  }
 };
 
 /**
@@ -99,43 +106,47 @@ export const updateEvent = async (req: Request, res: Response) => {
  *
  * Now can be deleted as its not used
  */
+// TODO: test this with an actual token (DO NOT MERGE)
 export const registerUser = async (req: Request, res: Response) => {
-  try {
-    const { eventId } = req.params;
+    try {
+        const { eventId } = req.params;
+        let userId = req.userId;
+        const { registrationDate, paymentStatus, paymentDetails } = req.body;
+        // TODO: modify so it adds an actual user (?)
+        if (userId == null) {
+            userId = uuidv4();
+        }
 
-    const { userId, registrationDate, paymentStatus, paymentDetails } =
-      req.body;
+        const user = {
+            userId,
+            registrationDate,
+            paymentStatus,
+            paymentDetails,
+        };
 
-    const user = {
-      userId,
-      registrationDate,
-      paymentStatus,
-      paymentDetails,
-    };
+        const event = await Event.findById(eventId);
 
-    const event = await Event.findById(eventId);
+        const userAlreadyPresent = event?.users?.find(
+            (e: RegistrationRecordEvent) =>
+                e.userId.toHexString() === userId!.toString()
+        );
+        if (userAlreadyPresent) {
+            const err: Error = new Error();
+            err.message = "User already registered for this event";
+            throw err;
+        }
 
-    const userAlreadyPresent = event?.users?.find(
-      (e: RegistrationRecordEvent) =>
-        e.userId.toHexString() === userId.toString()
-    );
-    if (userAlreadyPresent) {
-      const err: Error = new Error();
-      err.message = "User already registered for this event";
-      throw err;
+        const updatedEvent = await Event.findByIdAndUpdate(
+            eventId,
+            { $addToSet: { users: user } },
+            { new: true }
+        );
+
+        res.status(200).json(updatedEvent);
+    } catch (error) {
+        console.error(error);
+        res.status(400).json(error);
     }
-
-    const updatedEvent = await Event.findByIdAndUpdate(
-      eventId,
-      { $addToSet: { users: user } },
-      { new: true }
-    );
-
-    res.status(200).json(updatedEvent);
-  } catch (error) {
-    console.error(error);
-    res.status(400).json(error);
-  }
 };
 
 /*
@@ -143,35 +154,35 @@ export const registerUser = async (req: Request, res: Response) => {
  *  Now can be deleted as its not used
  */
 export const removeUser = async (req: Request, res: Response) => {
-  try {
-    const { eventId } = req.params;
-    const { userId } = req.body;
+    try {
+        const { eventId } = req.params;
+        const { userId } = req.body;
 
-    const updatedEvent = await Event.findByIdAndUpdate(
-      eventId,
-      { $pull: { users: { userId: userId } } },
-      { new: true }
-    );
+        const updatedEvent = await Event.findByIdAndUpdate(
+            eventId,
+            { $pull: { users: { userId: userId } } },
+            { new: true }
+        );
 
-    res.status(200).json(updatedEvent);
-  } catch (error) {
-    console.error(error);
-    res.status(400).json(error);
-  }
+        res.status(200).json(updatedEvent);
+    } catch (error) {
+        console.error(error);
+        res.status(400).json(error);
+    }
 };
 
 export const deleteEvent = async (req: Request, res: Response) => {
-  const { eventId } = req.params;
+    const { eventId } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(eventId)) {
-    return res.status(404).json({ error: "No such event" });
-  }
+    if (!mongoose.Types.ObjectId.isValid(eventId)) {
+        return res.status(404).json({ error: "No such event" });
+    }
 
-  const event = await Event.findOneAndDelete({ _id: eventId });
+    const event = await Event.findOneAndDelete({ _id: eventId });
 
-  if (!event) {
-    return res.status(404).json({ error: "No such event" });
-  }
+    if (!event) {
+        return res.status(404).json({ error: "No such event" });
+    }
 
-  res.status(200).json(event);
+    res.status(200).json(event);
 };
---
+++ packages/backend/typings/express.d.ts
@@ -0,0 +1,5 @@
+declare namespace Express {
+    export interface Request {
+        userId?: string; // I use string for example, you can put other type
+    }
+}
