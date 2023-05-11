bd134d8e80fb4b1215285272d8972b4e7310f734
NoamBechhofer <noam.bechhofer@gmail.com>
2023-05-11 17:13:25 +1200

    fixed asynchronous flow

--- packages/backend/src/payment/payment-controller.ts
+++ packages/backend/src/payment/payment-controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { Event } from "../event/event-model";
-import { registerUserEvent, updatePaid } from '../register/register-controller'
+import { registerUserEvent } from '../register/register-controller'
+import { User } from "../user/user-model";
+import assert from "node:assert/strict"
 
 import dotenv from "dotenv";
 
@@ -17,14 +19,19 @@ import Stripe from "stripe";
 const stripe = new Stripe(`${process.env.STRIPE_SECRET_KEY}`, { apiVersion: '2022-11-15' });
 
 
+/**
+ * Backend controller for Stripe Checkout.
+ */
 export const checkout = async (req: Request, res: Response) => {
-    const { eventId } = req.params;
+
+    assert(req.params.eventId, "Missing req.params.eventId");
+    assert(req.params.userId, "Missing req.params.userId")
 
     // Retrieve the event data from MongoDB
-    const event = await Event.findById(eventId);
+    const event = await Event.findById(req.params.eventId);
 
     if (!event) {
-        throw new Error("Event doesn't exist");
+        throw new Error("Event does not exist");
     }
 
     const product = await stripe.products.retrieve(
@@ -34,10 +41,10 @@ export const checkout = async (req: Request, res: Response) => {
     if (!product.default_price) {
         console.log(product)
         throw new Error("Event does not have an associated default price");
-    } 
+    }
 
     // Create a Stripe Checkout Session
-    const session = await stripe.checkout.sessions.create({
+    await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
 
         line_items: [{
@@ -50,25 +57,70 @@ export const checkout = async (req: Request, res: Response) => {
         cancel_url: 'https://google.com', // TODO : significant URL eg 'https://your-frontend-url.com/payment-cancel',
 
         mode: 'payment',
-    });
+    }).then(async (session) => {
+        if (session.payment_status === 'unpaid') {
+            res.status(400).json({ message: 'Payment failed' });
+            return;
+        }
 
-    if (session.payment_status === 'unpaid') {
-        return res.status(400).json({ message: 'Payment failed' });
-    }
-
-    try {
+        // prep req for registerUserEvent()
+        // ! we should decide on a standard of whether userId and eventId should be in the body or params
         req.body.userId = req.params.userId;
         req.body.eventId = req.params.eventId;
-
         req.body.registrationDate = Date.now();
         req.body.paid = true;
         req.body.paymentDetails = "card";
 
-        await registerUserEvent(req, res);
-        updatePaid(req, res);
+        await registerUserEvent(req, res)
+            .then(async () => {
+
+                const { userId } = req.params;
+                const paid = true;
+
+                // Check if user is registered under the event
+                const eventData = await Event.findOne({
+                    _id: req.params.eventId,
+                    users: { $elemMatch: { userId: userId } },
+                });
+
+                // If registration already exists, update paid status, otherwise return error
+                if (eventData != null) {
+                    await Event.updateOne(
+                        {
+                            _id: req.params.eventId,
+                            "users.userId": userId,
+                        },
+                        {
+                            $set: { "users.$.paid": paid },
+                        }
+                    );
+
+                    await User.updateOne(
+                        {
+                            _id: userId,
+                            "events.eventId": req.params.eventId,
+                        },
+                        {
+                            $set: { "events.$.paid": paid },
+                        }
+                    );
+                    res.status(200).json({
+                        message: "Paid status has been updated",
+                    });
+                } else {
+                    throw new Error("Unable to update registration, it does not exist");
+                }
+            }).catch((error) => {
+                console.error(error);
+                res.status(400).json(error);
+                return;
+            });
+
         return res.status(200).json({ sessionId: session.id });
-    } catch (error) {
+
+    }).catch((error) => {
         console.error(error);
-        return res.status(400).json(error);
-    }
+        res.status(400).json(error);
+        return;
+    })
 }
