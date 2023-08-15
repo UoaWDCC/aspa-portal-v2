e2ed0ec96f2c314f471d9554c4dd9133ad53f934
Jason <79180247+Bobsters6666@users.noreply.github.com>
2023-08-16 11:36:14 +1200

    added filter to admin dashboard events

--- packages/frontend/src/routes/dashboard/admin/AdminEvents.jsx
+++ packages/frontend/src/routes/dashboard/admin/AdminEvents.jsx
@@ -5,6 +5,8 @@ import { FaTrash, FaArrowRight } from "react-icons/fa";
 
 export default function AdminEvents() {
   const [events, setEvents] = useState([]);
+  const [selectedFilter, setSelectedFilter] = useState("all");
+  const [filteredEvents, setFilteredEvents] = useState([]);
 
   // fetch events from backend
   useEffect(() => {
@@ -34,40 +36,75 @@ export default function AdminEvents() {
     }
   };
 
+  const filterEvents = (filterType) => {
+    setSelectedFilter(filterType);
+
+    if (filterType === "all") {
+      setFilteredEvents(events);
+    } else if (filterType === "upcoming") {
+      const currentDate = new Date();
+      const upcomingEvents = events.filter(
+        (event) => new Date(event.eventTime) > currentDate
+      );
+      setFilteredEvents(upcomingEvents);
+    } else if (filterType === "past") {
+      const currentDate = new Date();
+      const pastEvents = events.filter(
+        (event) => new Date(event.eventTime) < currentDate
+      );
+      setFilteredEvents(pastEvents);
+    }
+  };
+
   return (
     <div>
       <h1 className="text-4xl font-bold text-white ml-44 mt-12 mb-8">Events</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 w-9/12 mx-auto gap-8 relative">
-        {events.map((event) => (
+      <div className="text-center mb-8">
+        <button
+          className={`mr-4 ${selectedFilter === "all" ? "text-blue-500" : ""}`}
+          onClick={() => filterEvents("all")}
+        >
+          All Events
+        </button>
+        <button
+          className={`mr-4 ${
+            selectedFilter === "upcoming" ? "text-blue-500" : ""
+          }`}
+          onClick={() => filterEvents("upcoming")}
+        >
+          Upcoming Events
+        </button>
+        <button
+          className={`mr-4 ${selectedFilter === "past" ? "text-blue-500" : ""}`}
+          onClick={() => filterEvents("past")}
+        >
+          Past Events
+        </button>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 w-9/12 mx-auto gap-8">
+        {filteredEvents.map((event) => (
           <div
             key={event._id}
-            className="bg-gray-800 p-6 rounded-md shadow-md border border-gray-700 mb-4 relative "
+            className="bg-gray-800 p-6 rounded-md shadow-md border border-gray-700 mb-4 relative"
           >
-            <h2 className="text-2xl font-semibold text-white">
-              {event.eventTitle}
-            </h2>
-            <p className="text-gray-300 mt-2">
-              Description: {event.eventDescription}
-            </p>
-            <p className="text-gray-300">Location: {event.eventLocation}</p>
-            <p className="text-gray-300">Price: $7</p>
-            <p className="text-gray-300">
+            <div className="flex items-center justify-between mb-3">
+              <h2 className="text-2xl font-semibold text-white">
+                {event.eventTitle}
+              </h2>
+              <FaTrash
+                size={18}
+                className="text-white cursor-pointer hover:text-red-500 transition"
+                onClick={() => handleDeleteEvent(event._id)}
+              />
+            </div>
+            <p className="text-gray-400">
               Date: {new Date(event.eventTime).toLocaleDateString()}
             </p>
-            <p className="text-gray-300">
-              {event.completed ? "Status: Completed" : "Status: Ongoing"}
-            </p>
-            <FaTrash
-              size={20}
-              className="absolute top-4 right-4 text-white cursor-pointer hover:opacity-75"
-              onClick={() => handleDeleteEvent(event._id)}
-            />
-
-            {/* link to individual event page */}
+            <p className="text-gray-400">Price: $7</p>
             <a href={`events/${event._id}`}>
               <FaArrowRight
-                size={22}
-                className="absolute bottom-4  right-4 cursor-pointer hover:opacity-75"
+                size={18}
+                className="text-white cursor-pointer hover:text-blue-500 transition mt-6"
               />
             </a>
           </div>
